03ef3e26afcb6d07b850f2db81f4949534e25d2b
tora <x.o4o8.x@aol.jp>
2021-07-10 14:40:52 +0900

    Changed JS script.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,22 @@
 
 {
     const container = document.getElementById('main_container');
+    const popup = document.getElementById('popup')
     const img = document.getElementById('popup_img');
-    let products = document.getElementsByClassName('products');
 
-    container.addEventListener('click', (a) => {
-        let img_url = a.target.src; 
-        console.log(img_url);
-        img.src = img_url;
-        a.target.parentNode.classList.add('display');
+    container.addEventListener('click', (e) => {
+        console.log(popup.className);
+        if (popup.className === 'display-block')
+        {
+            popup.classList.remove('display-block');
+            popup.classList.add('display-none');
+        }
+        console.log(popup.className);
+        // let img_url = e.target.src; 
+        // console.log(img_url);
+        // img.src = img_url;
+        popup.classList.remove('display-none');
+        popup.classList.add('display-block');
     });
 }
 
