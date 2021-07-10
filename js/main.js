efba92bbb56a5fa0e6a3fdfd65f6ece2c8d9a2da
tora <x.o4o8.x@aol.jp>
2021-07-10 16:26:11 +0900

    Changed JS file.

--- js/main.js
+++ js/main.js
@@ -2,22 +2,29 @@
 
 {
     const container = document.getElementById('main_container');
-    const popup = document.getElementById('popup')
-    const img = document.getElementById('popup_img');
+    const popup = document.getElementById('popup');
+    const popup_title = document.getElementById('popup_title');
+    const close = document.getElementById('close');
 
     container.addEventListener('click', (e) => {
-        console.log(popup.className);
-        if (popup.className === 'display-block')
+        let children = popup.childNodes;
+        console.log(children);
+        console.log(children[5].tagName);
+        if (children[5].tagName === "IMG")
         {
+            children[5].remove();
+        }
+        const clone_img = e.target.cloneNode(true);
+        clone_img.classList.add('popup_img');
+        popup.insertBefore(clone_img, popup_title);
+        popup.classList.remove('display-none');
+        popup.classList.add('display-block'); // toggle?
+
+        close.addEventListener('click', () => {
+            clone_img.remove();
             popup.classList.remove('display-block');
             popup.classList.add('display-none');
-        }
-        console.log(popup.className);
-        // let img_url = e.target.src; 
-        // console.log(img_url);
-        // img.src = img_url;
-        popup.classList.remove('display-none');
-        popup.classList.add('display-block');
+        });
     });
 }
 
