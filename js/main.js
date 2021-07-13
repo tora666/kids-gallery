1f4a4dadf4a551dd707043e426f02ea620a7e976
tora <x.o4o8.x@aol.jp>
2021-07-13 16:42:31 +0900

    Chenged JS script.

--- js/main.js
+++ js/main.js
@@ -4,27 +4,56 @@
     const container = document.getElementById('main_container');
     const popup = document.getElementById('popup');
     const popup_title = document.getElementById('popup_title');
+    const popup_name = document.getElementById('creater_name');
     const close = document.getElementById('close');
 
     container.addEventListener('click', (e) => {
+        // 先に作られたimgがあれば削除する
         let children = popup.childNodes;
-        console.log(children);
-        console.log(children[5].tagName);
         if (children[5].tagName === "IMG")
         {
             children[5].remove();
         }
-        const clone_img = e.target.cloneNode(true);
+
+        // クリックした要素を複製して挿入する。クラスを付けて表示する
+        let clone_img = e.target.cloneNode(true);
         clone_img.classList.add('popup_img');
         popup.insertBefore(clone_img, popup_title);
         popup.classList.remove('display-none');
-        popup.classList.add('display-block'); // toggle?
+        popup.classList.add('display-block'); // toggleにすべき？
+        
+        // 作者名を入れる。srcから切り取って入れる。
+        let clone_name = e.target.src.split('/');
+        clone_name = (clone_name[clone_name.length - 1].split('_'))[0];
+        clone_name = Check_name(clone_name);
+        popup_name.textContent = clone_name;
 
+        // バツボタンで閉じる。
         close.addEventListener('click', () => {
             clone_img.remove();
             popup.classList.remove('display-block');
             popup.classList.add('display-none');
         });
+
+        // 作者名の振り分け用
+        function Check_name(name){
+            switch(name) {
+                case 'shizuku':
+                    return 'Shizuo';
+                    break;
+                case 'kai':
+                    return 'kai-Knight';
+                    break;
+                case 'sumire':
+                    return 'Sumii';
+                    break;
+                case 'yuzu':
+                    return 'Yu-san';
+                    break;
+                default:
+                    return 'だれか';
+            }
+        }
     });
 }
 
